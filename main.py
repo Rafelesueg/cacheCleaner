880eaa1805d7b510e24556ae559a4d65390d35ac
Raff <68927180+Rafelesueg@users.noreply.github.com>
2024-06-02 19:11:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,33 +2,28 @@ import os
 import shutil
 import time
 
+
+def find_fivem_directory():
+    app_name = "FiveM"
+    app_data_path = os.path.join(os.getenv("LOCALAPPDATA"), app_name)
+    if os.path.exists(app_data_path):
+        return app_data_path
+    return None
 def delete_folder():
     user = os.getlogin()
     prefix_cache = "cache"
     prefix_nui_storage = "nui-storage"
     prefix_server_cache = "server-cache"
     prefix_server_cache_priv = "server-cache-priv"
-    user_input = "null"
-    custom_path = False
+
+    fivem_directory = find_fivem_directory()
 
     print("cacheCleaner for FiveM made by Rafelesueg\nGithub:https://github.com/Rafelesueg\n\n")
 
-    check = input(f"Custom installation path? (y/n): ")
-    if check.lower() == "y":
-        print("EXAMPLE PATH= Users\{user}\AppData\Local\FiveM\FiveM.app\data MAKE SURE TO INCLUDE 'data' FOLDER ")
-        user_input = input("Insert here: ")
-        custom_path = True
-
-    if not custom_path:
-        path_cache = rf"C:\Users\{user}\AppData\Local\FiveM\FiveM.app\data\{prefix_cache}"
-        path_nui_storage = rf"C:\Users\{user}\AppData\Local\FiveM\FiveM.app\data\{prefix_nui_storage}"
-        path_server_cache = rf"C:\Users\{user}\AppData\Local\FiveM\FiveM.app\data\{prefix_server_cache}"
-        path_server_cache_priv = rf"C:\Users\{user}\AppData\Local\FiveM\FiveM.app\data\{prefix_server_cache_priv}"
-    else:
-        path_cache = os.path.join(user_input, prefix_cache)
-        path_nui_storage = os.path.join(user_input, prefix_nui_storage)
-        path_server_cache = os.path.join(user_input, prefix_server_cache)
-        path_server_cache_priv = os.path.join(user_input, prefix_server_cache_priv)
+    path_cache = os.path.join(fivem_directory, "FiveM.app\\data", prefix_cache)
+    path_nui_storage = os.path.join(fivem_directory, "FiveM.app\\data", prefix_nui_storage)
+    path_server_cache = os.path.join(fivem_directory, "FiveM.app\\data", prefix_server_cache)
+    path_server_cache_priv = os.path.join(fivem_directory, "FiveM.app\\data", prefix_server_cache_priv)
 
     confirm = input(f"Are you sure you want to delete caches? (y/n): ")
 
